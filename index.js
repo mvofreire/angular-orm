a23aefdbce6d5cc573051c96ae9022a2d61b5d89
Marcus Freire <vinny.freire@gmail.com>
2016-02-15 01:30:58 -0300

    Create Structure Initial

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-(function(r, w){
-	'use strict';
-	module.exports = require('./core.js');
-
-	if(r)
-	{
-		module.exports = {};
-	}
-	else
-	{
-		w.AngularORM = {};
-	}
-})((module && module.exports), window);
+'use strict';
+module.exports = require('./lib/core.js');
